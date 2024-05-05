3f15c79c45c7215f88e57210686ecca9fbf9bfcc
cayna <cayna@agapesistemas.com.br>
2024-05-05 20:24:24 -0300

    Initial Commit

--- target/classes/db/migration/V1__create-table-users.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users(
-                      id SERIAL PRIMARY KEY NOT NULL,
-                    name TEXT NOT NULL,
-                      login TEXT NOT NULL UNIQUE,
-                      password TEXT NOT NULL,
-                      role TEXT NOT NULL
-);
